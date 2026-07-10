49b6b4465b469c3f576beab2609e162ec811c3af
Junchao Chen <junchao.chen.sc@gmail.com>
2026-07-10 14:43:16 +0200

    WIP: split HPDCache-WB fence flush behavior

--- core/controller.sv
+++ core/controller.sv
@@ -85,6 +85,11 @@ module controller
   logic fence_active_d, fence_active_q;
   logic flush_dcache;
 
+  // A normal FENCE need not flush the single-core HPDCache-WB configuration,
+  // but FENCE.I must still write back data before invalidating the I-cache.
+  localparam bit FlushDcacheOnFenceI =
+      CVA6Cfg.DcacheFlushOnFence || (CVA6Cfg.DCacheType == config_pkg::HPDCACHE_WB);
+
   // ------------
   // Flush CTRL
   // ------------
@@ -142,7 +147,7 @@ module controller
       flush_icache_o         = 1'b1;
       // this is not needed in the case since we
       // have a write-through cache in this case
-      if (CVA6Cfg.DcacheFlushOnFence) begin
+      if (FlushDcacheOnFenceI) begin
         flush_dcache   = 1'b1;
         fence_active_d = 1'b1;
       end
@@ -150,7 +155,7 @@ module controller
 
     // this is not needed in the case since we
     // have a write-through cache in this case
-    if (CVA6Cfg.DcacheFlushOnFence) begin
+    if (FlushDcacheOnFenceI) begin
       // wait for the acknowledge here
       if (flush_dcache_ack_i && fence_active_q) begin
         fence_active_d = 1'b0;
@@ -242,7 +247,7 @@ module controller
   // ----------------------
   always_comb begin
     // halt the core if the fence is active
-    halt_o = halt_csr_i || halt_acc_i || (CVA6Cfg.DcacheFlushOnFence && fence_active_q);
+    halt_o = halt_csr_i || halt_acc_i || (FlushDcacheOnFenceI && fence_active_q);
   end
 
   // ----------------------
