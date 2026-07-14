008bf5e9f082459f1bff599110ca0d1ddf79006c
Junchao Chen <junchao.chen.sc@gmail.com>
2026-07-14 14:14:09 +0200

    fix: align FPU wrapper

--- core/fpu_wrap.sv
+++ core/fpu_wrap.sv
@@ -553,8 +553,8 @@ module fpu_wrap
         .tag_o         (fpu_trans_id_o),
         .out_valid_o   (fpu_out_valid),
         .out_ready_i   (fpu_out_ready),
-        .busy_o        (  /* unused */),
-        .early_valid_o ()
+        .busy_o        (  /* unused */)
+        //.early_valid_o ()
     );
 
     // Pack status flag into exception cause, tval ignored in wb, exception is always invalid
