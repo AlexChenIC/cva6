b8015a328e4c2b3884877bc4e9123b3a12a7de66
Junchao Chen <junchao.chen.sc@gmail.com>
2026-07-25 13:02:44 +0200

    test(ci): characterize master PMP failures

--- verif/regress/dv-riscv-mmu-sv32-test.sh
+++ verif/regress/dv-riscv-mmu-sv32-test.sh
@@ -28,8 +28,29 @@ if ! [ -n "$DV_SIMULATORS" ]; then
   DV_SIMULATORS=veri-testharness,spike
 fi
 
-cd verif/sim
-python3 cva6.py --testlist=../tests/testlist_riscv-mmu-sv32-arch-test-$DV_TARGET.yaml --target $DV_TARGET --iss_yaml=cva6.yaml --iss=$DV_SIMULATORS $DV_OPTS --linker=../tests/riscv-arch-test/riscv-target/spike/link.ld
-status=$?
-cd -
-exit $status
+pmp_tests=(
+  rv32_vm_pmp_check_level_0_s
+  rv32_vm_pmp_check_level_0_u
+  rv32_vm_pmp_check_level_1_s
+  rv32_vm_pmp_check_level_1_u
+  rv32_vm_pte_pmp_check_level_1_u
+  rv32_vm_pte_pmp_check_level_1_s
+  rv32_vm_pte_pmp_check_level_0_u
+  rv32_vm_pte_pmp_check_level_0_s
+)
+
+errors=0
+for test_name in "${pmp_tests[@]}"; do
+  echo "::group::PMP diagnostic: ${test_name}"
+  cd verif/sim
+  python3 cva6.py --testlist=../tests/testlist_riscv-mmu-sv32-arch-test-$DV_TARGET.yaml --test "$test_name" --target "$DV_TARGET" --iss_yaml=cva6.yaml --iss="$DV_SIMULATORS" $DV_OPTS --linker=../tests/riscv-arch-test/riscv-target/spike/link.ld
+  status=$?
+  cd -
+  if ((status != 0)); then
+    errors=$((errors + 1))
+  fi
+  echo "::endgroup::"
+done
+
+echo "PMP diagnostic summary: ${errors} failure(s) across ${#pmp_tests[@]} tests"
+exit "$errors"
